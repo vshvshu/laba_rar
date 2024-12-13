abe720c92512a7aabbb67a3066a83523739832a5
Vasilisa <vasilisa311005@gmail.com>
2024-12-13 11:55:47 +0500

    Открыли файл, узнали размер файла, добавили индекс = 7+7+6=20

--- main.cpp
+++ main.cpp
@@ -34,3 +34,21 @@ struct main_head
 };
 
 #pragma pack(pop)
+
+int main()
+{
+    ifstream file("C:\\study\\labRar\\Example.rar", ios::binary);
+    if(file.is_open())
+        {
+
+        file.seekg(0, ios::end);
+        int sizeFile = file.tellg();
+        file.seekg(0, ios::beg);
+
+        vector<char> array(sizeFile, 0);
+
+        file.read(array.data(), sizeFile);
+
+        int index = 20;
+        }
+}
