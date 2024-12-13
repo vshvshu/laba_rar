4163f65cd080d28fd4943fcbfc7281fdd80c6105
Vasilisa <vasilisa311005@gmail.com>
2024-12-13 11:52:24 +0500

    Сделаны структры, а также добавлены библиотеки

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
+#include <cstdint>
 using namespace std;
 
-int main()
+#pragma pack (push, 1)
+
+struct head
 {
-    cout << "Hello world!" << endl;
-    return 0;
-}
+    uint16_t header_crc;
+    char  header_type;
+    uint16_t header_flag;
+    uint16_t header_size;
+};
+
+struct file_head
+{
+    uint32_t pack_size;
+    uint32_t unp_size;
+    char host_OS;
+    uint32_t file_crc;
+    uint32_t file_time;
+    char unp_ver;
+    char metod;
+    uint16_t name_size;
+    uint32_t file_attr;
+};
+
+struct main_head
+{
+    uint16_t high_posav;
+    uint32_t pos_AV;
+};
+
+#pragma pack(pop)
